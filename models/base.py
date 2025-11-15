e28aa5210d05e269c669dfd5ddaee254d4c5a8bd
luizandref <luiz.filho2006@gmail.com>
2025-11-15 12:08:10 -0300

    add inimigo e base

---
+++ models/base.py
@@ -0,0 +1,8 @@
+#classe que serve para como modelo tanto para o personagem quanto para o inimigo
+class Atributos:
+    def __init__(self, nome, hp):
+        self.nome = nome
+        self.hp = hp
+
+
+
